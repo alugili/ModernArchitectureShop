5236d470ac53d30651a3b1e9b0a7bb68077c4665
Bassam Alugili <balugili@stratec.com>
2020-09-25 11:10:27 +0200

    Version 16.8.0 Preview 3.1

--- src/Store/ModernArchitectureShop.StoreApi/Startup.cs
+++ src/Store/ModernArchitectureShop.StoreApi/Startup.cs
@@ -59,7 +59,7 @@ namespace ModernArchitectureShop.StoreApi
                     options.UseSqlServer(Configuration.GetConnectionString("SqlConnection")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
