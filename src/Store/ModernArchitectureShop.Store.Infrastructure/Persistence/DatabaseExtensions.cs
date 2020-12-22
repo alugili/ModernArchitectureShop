00395b098c902ee48bada58037ee80b29d3b9d41
Alugili, Bassam <b.alugili@stratec.com>
2020-12-22 22:12:41 +0100

    remove the UI referance.

--- src/Store/ModernArchitectureShop.Store.Infrastructure/Persistence/DatabaseExtensions.cs
+++ src/Store/ModernArchitectureShop.Store.Infrastructure/Persistence/DatabaseExtensions.cs
@@ -1,7 +1,5 @@
-using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using ModernArchitectureShop.Store.Application.Persistence;
 
 namespace ModernArchitectureShop.Store.Infrastructure.Persistence
@@ -12,18 +10,8 @@ namespace ModernArchitectureShop.Store.Infrastructure.Persistence
         {
             using var scope = webHost.Services.CreateScope();
             var services = scope.ServiceProvider;
-            try
-            {
-                var itemRepository = services.GetRequiredService<T>();
-                itemRepository.SeedDatabase();
-            }
-
-            catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger<ShopUI.Program>>();
-                logger.LogError(ex, ex.StackTrace);
-            }
-
+            var itemRepository = services.GetRequiredService<T>();
+            itemRepository.SeedDatabase();
             return webHost;
         }
     }
